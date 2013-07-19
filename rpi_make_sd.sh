1b7e2de2c3f2313002dc2601eaf7c685d4d53498
Alexandre Bulté <alexandre@bulte.net>
2013-07-19 12:57:48 +0200

    Simple script to burn an IMG on a sdcard (mac os)

---
+++ rpi_make_sd.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+EXPECTED_ARGS=2
+
+NAME=$1
+IMAGE="$2"
+
+if [ $# -ne $EXPECTED_ARGS ]
+then
+    echo "Usage: `basename $0` <partition_name> <image_path>"
+    echo "Example: `basename $0` disk2s1 ./rpi.img"
+    exit $E_BADARGS
+fi
+echo
+
+echo "You're probably trying to use this disk:"
+df -h | grep $NAME
+echo
+echo
+
+DISK_NAME="r"${NAME:0:5}
+
+echo "Confirm you want to unmount /dev/$1 and erase /dev/$DISK_NAME by typing 'Yes, sir'"
+read confirmation
+
+if [ "$confirmation" != 'Yes, sir' ]
+then
+    echo "Abort!"
+    exit 0
+fi
+
+echo
+echo "sudo diskutil unmount /dev/$NAME"
+sudo diskutil unmount /dev/$NAME
+echo
+
+if [ ! -f "$2" ]
+then
+    echo "File $2 does not exist."
+    exit 0
+fi
+
+echo "sudo dd bs=1m if="$2" of=/dev/$DISK_NAME"
+echo "Please wait, might take a long time..."
+sudo dd bs=1m if="$2" of=/dev/$DISK_NAME
+echo 
+echo "sudo diskutil eject /dev/$DISK_NAME"
+sudo diskutil eject /dev/$DISK_NAME
+echo
+echo "Well done my friend \o/ Cheers."
+exit 0
